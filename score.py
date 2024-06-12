94bcb0e91ec3a2b88c1a74cafcd5e63f3dc46fd6
KeerDongfang <139739167+KeerDongfang@users.noreply.github.com>
2024-06-12 14:06:14 +0800

    Dongfang is wokring on this

--- score.py
+++ score.py
@@ -2,3 +2,7 @@
 
 score = 0
 
+score_text = font.render(f'Score: {score}', True, white)
+score_rect = score_text.get_rect()
+score_rect.topright = (SCREEN_WIDTH - 10, 10)
+screen.blit(score_text, score_rect)
